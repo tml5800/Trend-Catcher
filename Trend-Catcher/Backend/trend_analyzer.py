72ee0ac9bd157bfe0f4fba61207846e80bdd4a2d
tml5800 <tml5800@psu.edu>
2025-04-01 15:39:52 +0000

    update

---
+++ Trend-Catcher/Backend/trend_analyzer.py
@@ -0,0 +1,12 @@
+def get_trends():
+    return {
+        "trends": [
+            {"hashtag": "#springfashion", "views": 1200000},
+            {"hashtag": "#viralrecipe", "views": 800000}
+        ]
+    }
+
+def compare_trends():
+    return {
+        "comparison": "Trend #springfashion is currently outperforming #viralrecipe by 40% more engagement."
+    }
